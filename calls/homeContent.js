126c775596d67e3592db07287c8c90e5e8286a86
gideon <gideon@hubtel.com>
2021-07-17 00:03:45 +0000

    fixed build

--- calls/homeContent.js
+++ calls/homeContent.js
@@ -1,7 +1,7 @@
 import Grid from "@material-ui/core/Grid";
 import { DataStore } from "aws-amplify";
 import { useState, useEffect } from "react";
-import { Bouquets } from "../media/models";
+import { Bouquets } from "../src/models";
 import { useRouter } from "next/router";
 
 export default function HomeContent() {
@@ -20,7 +20,6 @@ export default function HomeContent() {
     );
     return () => subscription.unsubscribe();
   }, []);
-
 
   return (
     <>
@@ -55,26 +54,31 @@ export default function HomeContent() {
               </sl-card>
             </Grid>
           );
-        }
-        if (slug === undefined) {
-          <Grid item xs={6} md={4} key={i}>
-            <sl-card class="card-header">
-              <div slot="header">
-                {item.title}{" "}
-                <sl-badge type="danger" pulse>
-                  ₵{item.amount}
-                </sl-badge>
-                <sl-badge class="avail" type="info">
-                  {item.availability}
-                </sl-badge>
-              </div>
+        } else {
+          return (
+            <Grid item xs={6} md={4} key={i}>
+              <sl-card class="card-header">
+                <div slot="header">
+                  {item.title}{" "}
+                  <sl-badge type="danger" pulse>
+                    ₵{item.amount}
+                  </sl-badge>
+                  <sl-badge class="avail" type="info">
+                    {item.availability}
+                  </sl-badge>
+                </div>
 
-              <img slot="image" src={`/images/${item.img}`} alt={item.title} />
-              <sl-button class="add" href={item.link}>
-                Add to cart
-              </sl-button>
-            </sl-card>
-          </Grid>;
+                <img
+                  slot="image"
+                  src={`/images/${item.img}`}
+                  alt={item.title}
+                />
+                <sl-button class="add" href={item.link}>
+                  Add to cart
+                </sl-button>
+              </sl-card>
+            </Grid>
+          );
         }
       })}
     </>
