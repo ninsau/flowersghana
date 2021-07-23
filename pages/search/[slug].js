d9298bdc122ac77822a37c031e6387e59664d438
gideon <gideon@hubtel.com>
2021-07-23 02:32:25 +0000

    fixed allignment with search

--- pages/search/[slug].js
+++ pages/search/[slug].js
@@ -1,30 +1,9 @@
-import { makeStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
 import SearchComponent from "../../components/staticpages/search";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(1),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-  },
-}));
-
 export default function Search() {
-  const classes = useStyles();
-
   return (
     <>
-      <div className={classes.root}>
-        <Grid container spacing={1}>
-          <Grid container item xs={12} spacing={3}>
-            <SearchComponent />
-          </Grid>
-        </Grid>
-      </div>
+      <SearchComponent />
     </>
   );
 }
