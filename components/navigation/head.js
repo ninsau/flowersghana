805c8fdeb0a63152c190286836a8334aeea9f740
gideon <gideon@hubtel.com>
2021-07-30 05:56:19 +0000

    attempting to fix

--- components/navigation/head.js
+++ components/navigation/head.js
@@ -43,6 +43,7 @@ export default function HeadComponent() {
           sizes="16x16"
         />
 
+        <meta charSet="utf-8" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1"
--- next.config.js
+++ next.config.js
@@ -2,16 +2,6 @@ const { createSecureHeaders } = require("next-secure-headers");
 
 module.exports = {
   async headers() {
-    return [{ source: "/(.*)", headers: createSecureHeaders() },  {
-      source: '/:all*(webp|js|css)',
-      locale: false,
-      headers: [
-        {
-          key: 'Cache-Control',
-          value: 'public, max-age=999999999, must-revalidate',
-        }
-      ],
-    }];
+    return [{ source: "/(.*)", headers: createSecureHeaders() }];
   },
 };
-
