af36aa2022b62af7cc8d9ed4f9454433264c1879
ninsau <gideon@hubtel.com>
2021-12-05 14:00:58 +0000

    Active: Performance Improvements

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import dynamic from "next/dynamic";
-import BackdropComponent from "../components/loader/backdrop";
+// import BackdropComponent from "../components/loader/backdrop";
+import Content from "../components/homepage/content"
 
 export default function Home() {
-  const Content = dynamic(() => import("../components/homepage/content"), {
-    loading: () => <BackdropComponent />,
-  });
+  // const Content = dynamic(() => import("../components/homepage/content"), {
+  //   loading: () => <BackdropComponent />,
+  // });
   return (
     <>
       <Content />
