988e233325e8709f7b35ba897c12bcb04dd698cb
ninsau <gideon@hubtel.com>
2021-12-17 04:29:40 +0000

    Active: Input Stuck Fix

--- components/navigation/scripts.js
+++ components/navigation/scripts.js
@@ -5,7 +5,6 @@ export default function ScriptsComponent() {
   return (
     <>
       <Script
-        strategy="lazyOnload"
         dangerouslySetInnerHTML={{
           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -15,7 +14,6 @@ export default function ScriptsComponent() {
         }}
       />
       <Script
-        strategy="lazyOnload"
         type="module"
         src="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.47/dist/shoelace.js"
       />
